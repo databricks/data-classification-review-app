43c20c0cc7a6cf63efa145a91952c761199f4b78
danielseong1 <daniel.seong@databricks.com>
2024-10-25 09:04:21 -0700

    update

---
+++ const.py
@@ -0,0 +1,15 @@
+# These constants are used to reference the results table. 
+# They MUST be the same as the constants defined in data-classification client
+
+RESULT_SCHEMA_NAME = "_data_classification"
+RESULT_TABLE_NAME = "_result"
+
+RESULT_TABLE_TIMESTAMP_KEY = "timestamp"
+RESULT_TABLE_SCAN_ID_KEY = "scan_id"
+RESULT_TABLE_SCHEMA_NAME_KEY = "schema_name"
+RESULT_TABLE_TABLE_NAME_KEY = "table_name"
+RESULT_TABLE_REVIEW_STATUS_KEY = "review_status"
+SUMMARY_COLUMN_NAME_KEY = "column_name"
+SUMMARY_PII_ENTITY_KEY = "pii_entity"
+SUMMARY_SAMPLES_KEY = "samples"
+SUMMARY_RATIONALES_KEY = "rationales"
