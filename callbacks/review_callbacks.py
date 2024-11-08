78511ca208aabc3443121fd93f6a555f4c699d2f
danielseong1 <daniel.seong@databricks.com>
2024-11-08 14:29:41 -0800

    initial

--- callbacks/review_callbacks.py
+++ callbacks/review_callbacks.py
@@ -1,126 +1,156 @@
 from clients.spark_client import SparkClient
+from clients.databricks_client import DatabricksClient
 
-def register_callbacks(app, spark_client: SparkClient):
-  from collections import defaultdict
-  import const
-  import dash
-  import dash_bootstrap_components as dbc
-  from components import intervals, grid, tabs, notification, actions
-  from utils import error_utils
 
-  @dash.callback(
-      dash.Input(intervals.initial_refresh_interval_id, "n_intervals"),
-  )
-  def refresh_cluster_initial(n_intervals):
-      try:
-        spark_client.refresh_cluster("initial-" + str(n_intervals))
-      except Exception as e:
-         app.logger.info(str(e))
-        
-  @dash.callback(
-      dash.Input(intervals.long_refresh_interval_id, "n_intervals"),
-  )
-  def refresh_cluster_long(n_intervals):
-      try:
-        spark_client.refresh_cluster("long-" + str(n_intervals))
-      except Exception as e:
-         app.logger.info(str(e))
+def register_callbacks(
+    app, spark_client: SparkClient, databricks_client: DatabricksClient
+):
+    from collections import defaultdict
+    import const
+    import dash
+    import dash_bootstrap_components as dbc
+    from components import intervals, grid, tabs, notification, actions, store
+    from utils import error_utils
 
-  @dash.callback(
-      dash.Output(grid.unreviewed_grid_id, "rowData"),
-      dash.Output(tabs.to_review_tab_header_id, "children"),
-      dash.Output(grid.reviewed_grid_id, "rowData", allow_duplicate=True),
-      dash.Output(tabs.reviewed_tab_header_id, "children", allow_duplicate=True),
-      dash.Output(notification.error_notificaton_id, "is_open"),
-      dash.Output(notification.error_notificaton_id, "children"),
-      dash.Input(actions.search_id, "n_submit"),
-      dash.State(actions.search_id, "value"),
-      prevent_initial_call=True,
-      running=[
-          (
-              dash.Output(notification.content_spinner_id, "spinner_style"),
-              {"display": "block"},
-              {"display": "none"},
-          ),
-          (
-              dash.Output(tabs.tabs_id, "style"),
-              {"visibility": "hidden"},
-              {"visibility": "visible"},
-          ),
-      ],
-  )
-  def input_submit(n_submit, value):
-      if value:
-          try:
-              updated_data = spark_client.get_datasets(value)
-              unreviewed_data = updated_data[0].to_dict("records")
-              reviewed_data = updated_data[1].to_dict("records")
-              return (
-                  unreviewed_data,
-                  f"{str(len(unreviewed_data))} Classifications to review",
-                  reviewed_data,
-                  f"{str(len(reviewed_data))} Classifications reviewed",
-                  False,
-                  "",
-              )
-          except Exception as e:
-            err = error_utils.reformat_error(e)
-            return (
-                [],
-                "0 Classifications to review",
-                [],
-                "0 Classifications reviewed",
-                True,
-                dash.html.P(err, className="error-toast-content"),
+    @dash.callback(
+        dash.Input(intervals.initial_refresh_interval_id, "n_intervals"),
+    )
+    def refresh_cluster_initial(n_intervals):
+        try:
+            spark_client.refresh_cluster("initial-" + str(n_intervals))
+        except Exception as e:
+            app.logger.info(str(e))
+
+    @dash.callback(
+        dash.Input(intervals.long_refresh_interval_id, "n_intervals"),
+    )
+    def refresh_cluster_long(n_intervals):
+        try:
+            spark_client.refresh_cluster("long-" + str(n_intervals))
+        except Exception as e:
+            app.logger.info(str(e))
+
+    @dash.callback(
+        dash.Output(grid.unreviewed_grid_id, "rowData"),
+        dash.Output(tabs.to_review_tab_header_id, "children"),
+        dash.Output(grid.reviewed_grid_id, "rowData", allow_duplicate=True),
+        dash.Output(tabs.reviewed_tab_header_id, "children", allow_duplicate=True),
+        dash.Output(notification.error_notificaton_id, "is_open"),
+        dash.Output(notification.error_notificaton_id, "children"),
+        dash.Input(actions.search_id, "n_submit"),
+        dash.State(actions.search_id, "value"),
+        prevent_initial_call=True,
+        running=[
+            (
+                dash.Output(notification.content_spinner_id, "spinner_style"),
+                {"display": "block"},
+                {"display": "none"},
+            ),
+            (
+                dash.Output(tabs.tabs_id, "style"),
+                {"visibility": "hidden"},
+                {"visibility": "visible"},
+            ),
+        ],
+    )
+    def input_submit(n_submit, value):
+        if value:
+            try:
+                updated_data = spark_client.get_datasets(value)
+                unreviewed_data = updated_data[0].to_dict("records")
+                reviewed_data = updated_data[1].to_dict("records")
+                return (
+                    unreviewed_data,
+                    f"{str(len(unreviewed_data))} Classifications to review",
+                    reviewed_data,
+                    f"{str(len(reviewed_data))} Classifications reviewed",
+                    False,
+                    "",
+                )
+            except Exception as e:
+                err = error_utils.reformat_error(e)
+                return (
+                    [],
+                    "0 Classifications to review",
+                    [],
+                    "0 Classifications reviewed",
+                    True,
+                    dash.html.P(err, className="error-toast-content"),
+                )
+
+    def update_review_status_and_return(value, status, selected_rows, token_data):
+        token_data = token_data or {}
+        _, schema, table = value.split(".")
+        try:
+            update_review_status_list = []
+            apply_review_results_list = []
+            for row in selected_rows:
+                update_review_status_list.append(
+                    {
+                        const.RESULT_TABLE_SCHEMA_NAME_KEY: schema,
+                        const.RESULT_TABLE_TABLE_NAME_KEY: table,
+                        const.SUMMARY_COLUMN_NAME_KEY: row[
+                            const.SUMMARY_COLUMN_NAME_KEY
+                        ],
+                        const.SUMMARY_PII_ENTITY_KEY: row[const.SUMMARY_PII_ENTITY_KEY],
+                        const.RESULT_TABLE_SCAN_ID_KEY: row[
+                            const.RESULT_TABLE_SCAN_ID_KEY
+                        ],
+                    }
+                )
+                apply_review_results_list.append(
+                    {
+                        "column_name": row[const.SUMMARY_COLUMN_NAME_KEY],
+                        "data_class": const.SENSITIVE_DATA_CLASS_MAP.get(
+                            row[const.SUMMARY_PII_ENTITY_KEY], 0
+                        ),
+                    }
+                )
+
+            if status == "applied_tag":
+                accepted_classifications = apply_review_results_list
+                rejected_classifications = None
+            else:
+                accepted_classifications = None
+                rejected_classifications = apply_review_results_list
+
+            _, latest_token, latest_token_expiration = (
+                databricks_client.apply_review_results(
+                    value,
+                    accepted_classifications,
+                    rejected_classifications,
+                    token_data.get("token"),
+                    token_data.get("token_expiration"),
+                )
             )
 
+            spark_client.update_review_status(value, update_review_status_list, status)
+            updated_data = spark_client.get_datasets(value)
+            unreviewed_data = updated_data[0].to_dict("records")
+            reviewed_data = updated_data[1].to_dict("records")
+            return (
+                unreviewed_data,
+                f"{str(len(unreviewed_data))} Classifications to review",
+                reviewed_data,
+                f"{str(len(reviewed_data))} Classifications reviewed",
+                False,
+                "",
+                {"token": latest_token, "token_expiration": latest_token_expiration},
+            )
+        except Exception as e:
+            err = error_utils.reformat_error(e)
+            return (
+                dash.no_update,
+                dash.no_update,
+                dash.no_update,
+                dash.no_update,
+                True,
+                dash.html.P(err, className="error-toast-content"),
+                dash.no_update,
+            )
 
-  def update_review_status_and_return(value, status, selected_rows):
-      _, schema, table = value.split(".")
-      updates_list = [
-          {
-              const.RESULT_TABLE_SCHEMA_NAME_KEY: schema,
-              const.RESULT_TABLE_TABLE_NAME_KEY: table,
-              const.SUMMARY_COLUMN_NAME_KEY: row[const.SUMMARY_COLUMN_NAME_KEY],
-              const.SUMMARY_PII_ENTITY_KEY: row[const.SUMMARY_PII_ENTITY_KEY],
-              const.RESULT_TABLE_SCAN_ID_KEY: row[const.RESULT_TABLE_SCAN_ID_KEY],
-          }
-          for row in selected_rows
-      ]
-      try:
-          if (status == "applied_tag"):
-            updates_dict = defaultdict(list)
-            for row in selected_rows:
-                updates_dict[row[const.SUMMARY_COLUMN_NAME_KEY]].append(row[const.SUMMARY_PII_ENTITY_KEY])
-            # TODO: Replace this with a service call once the service endpoint is ready
-            spark_client.apply_tags(value, updates_dict)
-
-          spark_client.update_review_status(value, updates_list, status)
-          updated_data = spark_client.get_datasets(value)
-          unreviewed_data = updated_data[0].to_dict("records")
-          reviewed_data = updated_data[1].to_dict("records")
-          return (
-              unreviewed_data,
-              f"{str(len(unreviewed_data))} Classifications to review",
-              reviewed_data,
-              f"{str(len(reviewed_data))} Classifications reviewed",
-              False,
-              "",
-          )
-      except Exception as e:
-          err = error_utils.reformat_error(e)
-          return (
-              dash.no_update,
-              dash.no_update,
-              dash.no_update,
-              dash.no_update,
-              True,
-              dash.html.P(err, className="error-toast-content"),
-          )
-
-
-  dash.clientside_callback(
-      """
+    dash.clientside_callback(
+        """
       function(selectedRows) {
           if (!!selectedRows && selectedRows.length > 0) {
               return [`Apply classification tags (${selectedRows.length})`, false,`Reject (${selectedRows.length})`, false]
@@ -128,62 +158,72 @@ def register_callbacks(app, spark_client: SparkClient):
           return ["Apply classification tags", true, "Reject", true]
       }
       """,
-      dash.Output(actions.apply_action_id, "children"),
-      dash.Output(actions.apply_action_id, "disabled"),
-      dash.Output(actions.reject_action_id, "children"),
-      dash.Output(actions.reject_action_id, "disabled"),
-      dash.Input(grid.unreviewed_grid_id, "selectedRows"),
-      prevent_initial_call=True,
-  )
+        dash.Output(actions.apply_action_id, "children"),
+        dash.Output(actions.apply_action_id, "disabled"),
+        dash.Output(actions.reject_action_id, "children"),
+        dash.Output(actions.reject_action_id, "disabled"),
+        dash.Input(grid.unreviewed_grid_id, "selectedRows"),
+        prevent_initial_call=True,
+    )
 
+    @dash.callback(
+        dash.Output(grid.unreviewed_grid_id, "rowData", allow_duplicate=True),
+        dash.Output(tabs.to_review_tab_header_id, "children", allow_duplicate=True),
+        dash.Output(grid.reviewed_grid_id, "rowData", allow_duplicate=True),
+        dash.Output(tabs.reviewed_tab_header_id, "children", allow_duplicate=True),
+        dash.Output(notification.error_notificaton_id, "is_open", allow_duplicate=True),
+        dash.Output(
+            notification.error_notificaton_id, "children", allow_duplicate=True
+        ),
+        dash.Output(store.token_store_id, "data", allow_duplicate=True),
+        dash.Input(actions.apply_action_id, "n_clicks"),
+        dash.State(grid.unreviewed_grid_id, "selectedRows"),
+        dash.State(actions.search_id, "value"),
+        dash.State(store.token_store_id, "data"),
+        prevent_initial_call=True,
+        running=[
+            (dash.Output(actions.apply_action_id, "disabled"), True, False),
+            (dash.Output(actions.reject_action_id, "disabled"), True, False),
+            (
+                dash.Output(actions.apply_action_id, "children"),
+                [dbc.Spinner(size="sm", spinner_class_name="me-2"), "Applying tags..."],
+                ["Apply classification tags"],
+            ),
+        ],
+    )
+    def apply_tags(n_clicks, selected_rows, value, data):
+        if n_clicks > 0 and len(selected_rows) > 0:
+            return update_review_status_and_return(
+                value, "applied_tag", selected_rows, data
+            )
 
-  @dash.callback(
-      dash.Output(grid.unreviewed_grid_id, "rowData", allow_duplicate=True),
-      dash.Output(tabs.to_review_tab_header_id, "children", allow_duplicate=True),
-      dash.Output(grid.reviewed_grid_id, "rowData", allow_duplicate=True),
-      dash.Output(tabs.reviewed_tab_header_id, "children", allow_duplicate=True),
-      dash.Output(notification.error_notificaton_id, "is_open", allow_duplicate=True),
-      dash.Output(notification.error_notificaton_id, "children", allow_duplicate=True),
-      dash.Input(actions.apply_action_id, "n_clicks"),
-      dash.State(grid.unreviewed_grid_id, "selectedRows"),
-      dash.State(actions.search_id, "value"),
-      prevent_initial_call=True,
-      running=[
-          (dash.Output(actions.apply_action_id, "disabled"), True, False),
-          (dash.Output(actions.reject_action_id, "disabled"), True, False),
-          (
-              dash.Output(actions.apply_action_id, "children"),
-              [dbc.Spinner(size="sm", spinner_class_name="me-2"), "Applying tags..."],
-              ["Apply classification tags"],
-          ),
-      ],
-  )
-  def apply_tags(n_clicks, selected_rows, value):
-      if n_clicks > 0 and len(selected_rows) > 0:
-          return update_review_status_and_return(value, "applied_tag", selected_rows)
-
-
-  @dash.callback(
-      dash.Output(grid.unreviewed_grid_id, "rowData", allow_duplicate=True),
-      dash.Output(tabs.to_review_tab_header_id, "children", allow_duplicate=True),
-      dash.Output(grid.reviewed_grid_id, "rowData", allow_duplicate=True),
-      dash.Output(tabs.reviewed_tab_header_id, "children", allow_duplicate=True),
-      dash.Output(notification.error_notificaton_id, "is_open", allow_duplicate=True),
-      dash.Output(notification.error_notificaton_id, "children", allow_duplicate=True),
-      dash.Input(actions.reject_action_id, "n_clicks"),
-      dash.State(grid.unreviewed_grid_id, "selectedRows"),
-      dash.State(actions.search_id, "value"),
-      prevent_initial_call=True,
-      running=[
-          (dash.Output(actions.apply_action_id, "disabled"), True, False),
-          (dash.Output(actions.reject_action_id, "disabled"), True, False),
-          (
-              dash.Output(actions.reject_action_id, "children"),
-              [dbc.Spinner(size="sm", spinner_class_name="me-2"), "Rejecting..."],
-              ["Reject"],
-          ),
-      ],
-  )
-  def reject(n_clicks, selected_rows, value):
-      if n_clicks > 0 and len(selected_rows) > 0:
-          return update_review_status_and_return(value, "rejected", selected_rows)
+    @dash.callback(
+        dash.Output(grid.unreviewed_grid_id, "rowData", allow_duplicate=True),
+        dash.Output(tabs.to_review_tab_header_id, "children", allow_duplicate=True),
+        dash.Output(grid.reviewed_grid_id, "rowData", allow_duplicate=True),
+        dash.Output(tabs.reviewed_tab_header_id, "children", allow_duplicate=True),
+        dash.Output(notification.error_notificaton_id, "is_open", allow_duplicate=True),
+        dash.Output(
+            notification.error_notificaton_id, "children", allow_duplicate=True
+        ),
+        dash.Output(store.token_store_id, "data", allow_duplicate=True),
+        dash.Input(actions.reject_action_id, "n_clicks"),
+        dash.State(grid.unreviewed_grid_id, "selectedRows"),
+        dash.State(actions.search_id, "value"),
+        dash.State(store.token_store_id, "data"),
+        prevent_initial_call=True,
+        running=[
+            (dash.Output(actions.apply_action_id, "disabled"), True, False),
+            (dash.Output(actions.reject_action_id, "disabled"), True, False),
+            (
+                dash.Output(actions.reject_action_id, "children"),
+                [dbc.Spinner(size="sm", spinner_class_name="me-2"), "Rejecting..."],
+                ["Reject"],
+            ),
+        ],
+    )
+    def reject(n_clicks, selected_rows, value, data):
+        if n_clicks > 0 and len(selected_rows) > 0:
+            return update_review_status_and_return(
+                value, "rejected", selected_rows, data
+            )
--- const.py
+++ const.py
@@ -1,4 +1,4 @@
-# These constants are used to reference the results table. 
+# These constants are used to reference the results table.
 # They MUST be the same as the constants defined in data-classification client
 
 RESULT_SCHEMA_NAME = "_data_classification"
@@ -13,3 +13,22 @@ SUMMARY_COLUMN_NAME_KEY = "column_name"
 SUMMARY_PII_ENTITY_KEY = "pii_entity"
 SUMMARY_SAMPLES_KEY = "samples"
 SUMMARY_RATIONALES_KEY = "rationales"
+
+# This must be the same as `SensitiveDataClass` defined in data classification service proto
+SENSITIVE_DATA_CLASS_MAP = {
+    "sensitive_data_class_unspecified": 0,
+    "credit_card": 1,
+    "crypto": 2,
+    "email_address": 3,
+    "iban_code": 4,
+    "ip_address": 5,
+    "location": 6,
+    "person": 7,
+    "phone_number": 8,
+    "medical_license": 9,
+    "us_bank_number": 10,
+    "us_driver_license": 11,
+    "us_itin": 12,
+    "us_passport": 13,
+    "us_ssn": 14,
+}
